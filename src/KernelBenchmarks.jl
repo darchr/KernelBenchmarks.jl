3e7b06fde0b02a9dde787dec55da16132a875b5f
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-03-18 09:09:03 -0700

    Files generated by PkgTemplates

---
+++ src/KernelBenchmarks.jl
@@ -0,0 +1,5 @@
+module KernelBenchmarks
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KernelBenchmarks
+using Test
+
+@testset "KernelBenchmarks.jl" begin
+    # Write your own tests here.
+end
